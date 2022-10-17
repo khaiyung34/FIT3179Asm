e309a9eeaf3a91b89cfed4972711f814a537aae9
khaiyung34 <111415343+khaiyung34@users.noreply.github.com>
2022-10-17 23:39:36 +1100

    Add files via upload

---
+++ html/vega_lite_vis.js
@@ -0,0 +1,19 @@
+var vg_1 = "map.json";
+
+vegaEmbed("#map", vg_1).then(function(result){}).catch(console.error);
+
+var vg_2 = "scatter.json";
+
+vegaEmbed("#scatter", vg_2).then(function(result){})
+
+var vg_3 = "agegroupvsdeath.json";
+
+vegaEmbed("#agegroupvsdeath",vg_3).then(function(result){})
+
+var vg_3 = "bar.json";
+
+vegaEmbed("#bar",vg_3).then(function(result){})
+
+var vg_3 = "doubleBar.json";
+
+vegaEmbed("#doubleBar",vg_3).then(function(result){})
